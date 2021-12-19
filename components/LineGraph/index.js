e3f2fe8712c4b2209b5d8ac8738990d94e1d5452
Alix <acui@stanford.edu>
2021-12-19 14:24:57 -0600

    Update light mode tooltip bg

--- components/LineGraph/index.js
+++ components/LineGraph/index.js
@@ -7,7 +7,7 @@ const ChartTooltip = ({ point, theme }) => {
     <div
       className={
         "flex flex-col items-center p-1 rounded-md " +
-        (theme === "dark" ? "dark-background" : "bg-gray-0")
+        (theme === "dark" ? "dark-background" : "bg-gray-50")
       }
     >
       <div className="flex items-center">
