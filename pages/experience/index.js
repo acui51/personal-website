435340610bcb4d294c18ff55f2dc2d79e47a85d5
acui51 <acui@stanford.edu>
2022-10-24 17:34:20 -0700

    Updates

--- pages/experience/index.js
+++ pages/experience/index.js
@@ -55,7 +55,7 @@ const Experience = () => {
               "Developed an entirely new page for product updates that utilizes Contentful CMS to serve content",
               "Full stack development for our people and directory product utilizing Go, React, and TypeScript",
               "Improved the consistency and robustness of our design system by utilizing Storybook and better categorizing our components in the codebase",
-              "Met some of the most talented, nicest, and funniest engineers ever 😄"
+              "Met some of the most talented, nicest, and funniest engineers ever 😄",
             ]}
             color={theme === "light" ? "#333CED" : "#FFFFFFE6"}
             technologies={[
@@ -64,14 +64,14 @@ const Experience = () => {
               "Redux",
               "Vanilla Extract",
               "Webpack",
-              "Go"
+              "Go",
             ]}
           />
           <ExperienceBullet
             company="Kleiner Perkins"
             occupation="Engineering Fellow"
             dates="Jun 2022 - "
-            description={["Fellowship"]}
+            description={["Fellowship", "Met a bunch of cool people on a boat"]}
             color={theme === "light" ? "#000000" : "#FFFFFFE6"}
           />
           <ExperienceBullet
@@ -82,7 +82,7 @@ const Experience = () => {
               "Co-led and taught Stanford's CS47: Cross-platform Mobile App Development in React Native.",
               "Gave lectures on React Native topics such as JavaScript basics, hooks, and React Navigation.",
               "Created two new assignments from scratch.",
-              "Held weekly office hours."
+              "Held weekly office hours.",
             ]}
             technologies={["React Native", "Expo"]}
             color="#B83A4B"
@@ -98,14 +98,14 @@ const Experience = () => {
               "RSpec",
               "Python",
               "Selenium",
-              "AWS suite"
+              "AWS suite",
             ]}
             description={[
               "Created the entire front-end of an internal tool that lets PMs and engineers grant API and Console permissions of varying degrees to multiple EC2 users at once.",
               "Conducted user interviews, created mock ups in Figma, and polled project stakeholders weekly to ensure design aligned with user needs.",
               "Developed the entire front-end using Ruby on Rails for templating HTML, jQuery for dynamic client-side logic, and SCSS for styling and responsiveness.",
               "Wrote unit and integration tests with RSpec, Python and Selenium with 90% code coverage percentage.",
-              "Contributed to the implementation of a Slackbot notification system using AWS suite (SNS, Chatbot, CloudMetrics)."
+              "Contributed to the implementation of a Slackbot notification system using AWS suite (SNS, Chatbot, CloudMetrics).",
             ]}
             color="rgb(255, 153, 0)"
           />
@@ -117,7 +117,7 @@ const Experience = () => {
               "Engineered the new author byline and author bio components used across almost every article using Next.js and Storybook.",
               "Created an author follow module that lets users receive notifications every time an article by the given author is published.",
               "Wrote unit tests using Jest and Enzyme and end-to-end tests using Cypress to maintain 80% code coverage.",
-              "Worked in agile development following scrum methodologies."
+              "Worked in agile development following scrum methodologies.",
             ]}
             technologies={["Next.js", "Jest", "Enzyme", "Cypress", "Storybook"]}
             dates="Mar 2021 - June 2021"
@@ -130,13 +130,13 @@ const Experience = () => {
               "Converted a logic courseware application to a newly improved web-based application using the MVC design pattern",
               "Refashioned the model component of the design pattern by serializing data from the backend into abstracted components",
               "Integrated REST API endpoints with Java using Jersey",
-              "Migrated from Bootstrap V3 to Bootstrap V4 with a complete responsive redesign of the application chrome"
+              "Migrated from Bootstrap V3 to Bootstrap V4 with a complete responsive redesign of the application chrome",
             ]}
             technologies={[
               "JavaScript (ES6)",
               "jQuery",
               "Java",
-              "Subversion SVN"
+              "Subversion SVN",
             ]}
             dates="Jun 2020 - April 2021"
           />
@@ -154,7 +154,7 @@ const Experience = () => {
               >
                 {" "}
                 Carta V2{" "}
-              </a>
+              </a>,
             ]}
             technologies={["React", "Redux", "TypeScript", "Storybook"]}
             dates="Sep 2020 - Present"
